85eafc0e96f38fa978d656d164508dda583e85bd
raokun <1043623941@qq.com>
2023-03-03 00:34:16 +0800

    commit

---
+++ raoStudy/SearchService.Domain/ISearchRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using SearchService.Domain.Objects;
+
+namespace SearchService.Domain
+{
+    public interface ISearchRepository
+    {
+        #region  DemoObj
+        public Task UpdateAsync(DemoObj obj);
+        public Task DeleteAsync(DemoObj obj);
+
+        public Task<DemoObjVo> SearchDemoObjs(string keyWord, int page, int pageSize);
+        #endregion
+    }
+}
